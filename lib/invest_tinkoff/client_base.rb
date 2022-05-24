c0f329d48f2cb2f0a88d018cd492303f1ad231f7
blackchestnut <alexander.kalinichev@gmail.com>
2022-05-24 19:57:00 +0300

    Extract module

--- lib/invest_tinkoff/client_base.rb
+++ lib/invest_tinkoff/client_base.rb
@@ -1,51 +1,53 @@
 # frozen_string_literal: true
 
-class InvestTinkoff::ClientBase
-  include HTTParty
+module InvestTinkoff
+  class ClientBase
+    include HTTParty
 
-  def initialize token:, broker_account_id:, logger: nil
-    @token = token
-    @broker_account_id = broker_account_id
-    @logger = logger
-  end
+    def initialize token:, broker_account_id:, logger: nil
+      @token = token
+      @broker_account_id = broker_account_id
+      @logger = logger
+    end
 
-  private
+    private
 
-  def get_api_request path, query = nil
-    response = self.class.get(
-      path,
-      query: query || base_query,
-      logger: @logger,
-      headers: headers
-    )
-    parse_response response
-  end
+    def get_api_request path, query = nil
+      response = self.class.get(
+        path,
+        query: query || base_query,
+        logger: @logger,
+        headers: headers
+      )
+      parse_response response
+    end
 
-  def post_api_request path, body: {}, query: {}
-    response = self.class.post(
-      path,
-      query: base_query.merge(query),
-      body: body.to_json,
-      logger: @logger,
-      headers: headers
-    )
-    parse_response response
-  end
+    def post_api_request path, body: {}, query: {}
+      response = self.class.post(
+        path,
+        query: base_query.merge(query),
+        body: body.to_json,
+        logger: @logger,
+        headers: headers
+      )
+      parse_response response
+    end
 
-  def headers
-    {
-      'Authorization' => "Bearer #{@token}",
-      'Content-Type' => 'application/json'
-    }
-  end
+    def headers
+      {
+        'Authorization' => "Bearer #{@token}",
+        'Content-Type' => 'application/json'
+      }
+    end
 
-  def base_query
-    return {} if @broker_account_id.nil?
+    def base_query
+      return {} if @broker_account_id.nil?
 
-    { brokerAccountId: @broker_account_id }
-  end
+      { brokerAccountId: @broker_account_id }
+    end
 
-  def parse_response response
-    response.parsed_response
+    def parse_response response
+      response.parsed_response
+    end
   end
 end
